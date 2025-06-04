19e31aabdc40ab2530fdf7350a362a3b4e71160f
ayyaistnn <164536096+ayyaistnn@users.noreply.github.com>
2025-06-04 10:03:11 +0700

    update

---
+++ LAB/02-PythonTest.py
@@ -0,0 +1,4 @@
+name = input("What is your name? ")
+like = ("What do you like? ")
+
+print(name + " like " + ".")
---
+++ LAB/03-PythonTest.py
@@ -0,0 +1,2 @@
+print(f"{team1} versus {team2}")
+print(f"Final score: {score1} to {score2}")
